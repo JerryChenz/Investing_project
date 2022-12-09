ab87893204ed4562a8fa4bb03c6f1722c1e39830
Jerry.chen <jerrychen.works@gmail.com>
2022-12-09 10:28:11 +0800

    Updated the get_forex_rate docstr

--- scrap_mod.py
+++ scrap_mod.py
@@ -7,7 +7,7 @@ from forex_python.converter import CurrencyRates
 
 
 def get_forex_rate(buy, sell):
-    """get exchange rate, buy is ask and sell is bid"""
+    """get exchange rate, buy means ask and sell means bid"""
 
     if buy != sell:
         c = CurrencyRates()
